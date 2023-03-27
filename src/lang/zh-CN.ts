97e23bf95d7c38daa087a5f06c5edba00c0a9d0c
xxxsf <xushunfa459@hellobike.com>
2023-03-27 14:05:02 +0800

    feat: 优化文档

--- src/lang/zh-CN.ts
+++ src/lang/zh-CN.ts
@@ -1,6 +1,7 @@
 export default {
   homeTitle: "下一代浏览器原生组件库",
-  homeSubtitle: "它可以同时在任意框架或无框架中使用（ps: 无框架组件库）",
+  homeSubtitle:
+    "它可以同时在任意框架或无框架中使用（跨技术栈组件库、无框架组件库）",
   getStarted: "开始",
   whyQuark: "为什么选择 Quark？",
   scrollDown: "Scroll Down",
