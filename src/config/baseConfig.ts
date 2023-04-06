f407a85a94914a38a828676c77f4b0bbdf053f7d
xxxsf <allan91coder@gmail.com>
2023-04-06 16:28:33 +0800

    feat: update

--- src/config/baseConfig.ts
+++ src/config/baseConfig.ts
@@ -18,4 +18,35 @@ export default {
       pathEnName: "#/en-US/component/button", // TODO: 临时方案
     },
   ],
+  docs: {
+    name: "指南",
+    enName: "Essentials",
+    packages: [
+      {
+        name: "Introduction",
+        cName: "介绍",
+        show: true,
+      },
+      {
+        name: "QuickStart",
+        cName: "快速上手",
+        show: true,
+      },
+      {
+        name: "InternationalAndTheme",
+        cName: "国际化/主题",
+        show: true,
+      },
+      {
+        name: "Notice",
+        cName: "常见问题",
+        show: true,
+      },
+      {
+        name: "CHANGELOG",
+        cName: "更新日志",
+        show: true,
+      },
+    ],
+  },
 };
